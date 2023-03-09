44357fa79d374caf05f81be3415464c5191158e7
Vladislav Mamon <norskeld@gmail.com>
2023-03-09 22:50:14 +0300

    formula(rsx): update `rsx` formula

--- Formula/rsx.rb
+++ Formula/rsx.rb
@@ -1,9 +1,9 @@
 class Rsx < Formula
-  version "0.2.0"
+  version "0.2.1"
   desc "Micro CLI for interactive execution of npm & yarn scripts."
   homepage "https://github.com/norskeld/rsx"
-  url "https://github.com/norskeld/rsx/releases/download/v0.2.0/rsx-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "11500b0bfe9fce86b425b13e5e9573c7e54914ba23383f2a156115ddca2c0622"
+  url "https://github.com/norskeld/rsx/releases/download/v0.2.1/rsx-0.2.1-x86_64-apple-darwin.tar.gz"
+  sha256 "6ed81c873a34ee0ec308d1890bc13d56135168705999dba8901434fb4a8c65bc"
   license "MIT"
 
   def install
