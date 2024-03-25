572e28597fe3161dc30fdb7b8f80ba59fd068c10
Vladislav Mamon <norskeld@gmail.com>
2024-03-25 13:37:30 +0300

    formula(diceware): update `diceware` formula

--- Formula/diceware.rb
+++ Formula/diceware.rb
@@ -1,9 +1,9 @@
 class Diceware < Formula
-  version "1.0.0"
+  version "1.0.1"
   desc "Rust crate and CLI for generating Diceware passphrases."
   homepage "https://github.com/norskeld/diceware"
-  url "https://github.com/norskeld/diceware/releases/download/v1.0.0/diceware-1.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "ef60c3439d694244e250f9b00b05ac156a11661b6f2fe7a0d8544d73ab034c2b"
+  url "https://github.com/norskeld/diceware/releases/download/v1.0.1/diceware-1.0.1-x86_64-apple-darwin.tar.gz"
+  sha256 "a3d15195a9d742bd592029269d06bc325ac172f498a2eaf473312a639ef80cea"
   license "MIT"
 
   def install
